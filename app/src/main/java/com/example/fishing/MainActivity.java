b95076dd1d089279829f410e6b5f0cf527822641
l1244042551 <rl17@illinois.edu>
2020-04-28 16:20:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/fishing/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.fishing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.fishing.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Tell the Login button what to do.
+        Login = findViewById(R.id.LoginButton);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open the Login Layout.
+                openLoginActivity();
+            }
+        });
+    }
+    // Create a method to open the LoginActivity.
+    public void openLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
