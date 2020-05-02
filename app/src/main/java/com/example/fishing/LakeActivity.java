d5cd19151739eeb6ec14d0321b952ea83b524f23
taige2 <taige2@illinois.edu>
2020-05-02 18:28:41 -0500

    initial pictures updated.

--- app/src/main/java/com/example/fishing/LakeActivity.java
+++ app/src/main/java/com/example/fishing/LakeActivity.java
@@ -5,12 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class LakeActivity extends AppCompatActivity {
     private ImageView lake;
+
+    private boolean[][] condition = new boolean[3][3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lake);
+
+        createFish();
+
+    }
+
+    private void createFish() {
+        int fishLocationX = new Random().nextInt(3);
+        int fishLocationY = new Random().nextInt(3);
+        condition[fishLocationX][fishLocationY] = true;
     }
 }
