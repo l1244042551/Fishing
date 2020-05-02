ba10946b3b901d0217bb7f2e8a4a72510918757c
l1244042551 <rl17@illinois.edu>
2020-05-02 18:49:18 -0500

    lol

--- app/src/main/java/com/example/fishing/SnowActivity.java
+++ app/src/main/java/com/example/fishing/SnowActivity.java
@@ -27,18 +27,70 @@ public class SnowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snow);
-        spot00 = findViewById(R.id.Spot00);
-        spot01 = findViewById(R.id.Spot01);
-        spot02 = findViewById(R.id.Spot02);
-        spot10 = findViewById(R.id.Spot10);
-        spot11 = findViewById(R.id.Spot11);
-        spot12 = findViewById(R.id.Spot12);
-        spot20 = findViewById(R.id.Spot20);
-        spot21 = findViewById(R.id.Spot21);
-        spot22 = findViewById(R.id.Spot22);
-        caught(false);
-
         createFish();
+        spot00 = findViewById(R.id.Spot00);
+        spot00.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                caught(condition[0][0]);
+            }
+        });
+        spot01 = findViewById(R.id.Spot01);
+        spot01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                caught(condition[0][1]);
+            }
+        });
+        spot02 = findViewById(R.id.Spot02);
+        spot02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                caught(condition[0][2]);
+            }
+        });
+        spot10 = findViewById(R.id.Spot10);
+        spot10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                caught(condition[1][0]);
+            }
+        });
+        spot11 = findViewById(R.id.Spot11);
+        spot11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                caught(condition[1][1]);
+            }
+        });
+        spot12 = findViewById(R.id.Spot12);
+        spot12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                caught(condition[1][2]);
+            }
+        });
+        spot20 = findViewById(R.id.Spot20);
+        spot20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                caught(condition[2][0]);
+            }
+        });
+        spot21 = findViewById(R.id.Spot21);
+        spot21.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                caught(condition[2][1]);
+            }
+        });
+        spot22 = findViewById(R.id.Spot22);
+        spot22.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                caught(condition[2][2]);
+            }
+        });
     }
 
     private void createFish() {
