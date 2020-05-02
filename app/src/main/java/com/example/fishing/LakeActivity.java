86adaef551789d3a4ea948e7802837ec00db4fc1
l1244042551 <rl17@illinois.edu>
2020-05-02 18:27:09 -0500

    lol

--- app/src/main/java/com/example/fishing/LakeActivity.java
+++ app/src/main/java/com/example/fishing/LakeActivity.java
@@ -1,5 +1,6 @@
 package com.example.fishing;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,5 +13,16 @@ public class LakeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lake);
+    }
+    public void caught(boolean caught) {
+        if (caught) {
+            AlertDialog.Builder success = new AlertDialog.Builder(this);
+            success.setMessage("Congratulations! You have caught this ");
+            success.create().show();
+        } else {
+            AlertDialog.Builder fail = new AlertDialog.Builder(this);
+            fail.setMessage("Oops, the fish got way. Good luck next time!");
+            fail.create().show();
+        }
     }
 }
--- app/src/main/java/com/example/fishing/SeaActivity.java
+++ app/src/main/java/com/example/fishing/SeaActivity.java
@@ -1,5 +1,6 @@
 package com.example.fishing;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,16 @@ public class SeaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sea);
+    }
+    public void caught(boolean caught) {
+        if (caught) {
+            AlertDialog.Builder success = new AlertDialog.Builder(this);
+            success.setMessage("Congratulations! You have caught this ");
+            success.create().show();
+        } else {
+            AlertDialog.Builder fail = new AlertDialog.Builder(this);
+            fail.setMessage("Oops, the fish got way. Good luck next time!");
+            fail.create().show();
+        }
     }
 }
